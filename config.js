c6012a041f59c126acd5d09e07b1665ef81369bf
boru-to <borutovortex49@gmail.com>
2024-09-07 06:33:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
-//Bot by 𝕶𝖎𝖓𝖌 𝕾𝖆𝖒//
+//Bot by ~katakuri//
 
 
 require("./all/module")
-global.owner = "254742491666" 
+global.owner = "27747815326" 
 global.namaCreator = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
 global.autoJoin = false 
 global.antilink = false 
 global.versisc = '6.0' 
-global.codeInvite = "https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e"
-global.isLink = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
+global.codeInvite = "https://whatsapp.com/channel/0029Vaf5x0eHFxP7JvSRRn1g"
+global.isLink = 'https://whatsapp.com/channel/0029Vaf5x0eHFxP7JvSRRn1g'
 global.sessionName = 'session'
 global.thumb = fs.readFileSync("./thumb.png") 
 global.audionya = fs.readFileSync("./all/sound.mp3") 
-global.packname = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
-global.author = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
+global.packname = "~katakuri" 
+global.author = "~katakuri-𝛁6" 
 global.jumlah = "5" 
-global.namabot = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
+global.namabot = "~katakuri-𝛁6" 
 global.domain = 'https://kingsam.sellerpanelerol.me' // your domian
 global.apikey = 'ptla_VRxkhvalZQ3EhHyAax9QYxgT5cbsx2MrO69gBpVZmmd' // Isi Apikey Plta Lu
 global.capikey = 'ptlc_sQ3d4GxCiztyV5DbhUJSWtvnSCyKzeMXoWIr2lVyqal' // Isi Apikey Pltc Lu
